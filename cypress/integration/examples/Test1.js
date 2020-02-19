8dc755ab11173ce43fc39803e873ecfced6471db
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-19 14:27:49 -0600

    implement second test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,12 @@
+import React from 'react';
+<reference types="Cypress" />;
+
 describe('My First Test Suite', function() {
   it('My First Test case', function() {
     // test step
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.get('.search-keyword').type('ca');
+    // fixture
   });
   it('My Second Test case', function() {
     // test step
