f375856ed25b5466252c50d2e616b5ac0c291742
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-19 15:01:07 -0600

    add more assertions

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,6 +6,8 @@ describe('My First Test Suite', function() {
     // test step
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca');
+    cy.wait(2000);
+    cy.get('.product:visible').should('have.length', 4);
     // fixture
   });
   it('My Second Test case', function() {
