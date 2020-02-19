96ebf453bcf387d41b5cc32b5ade610562ec49e9
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-18 23:24:20 -0600

    implement first test step

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,7 @@
 describe('My First Test Suite', function() {
   it('My First Test case', function() {
     // test step
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
   });
   it('My Second Test case', function() {
     // test step
