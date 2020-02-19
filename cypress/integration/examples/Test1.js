990ca8d61006d0353402e6e68e1670c345b2c425
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-18 23:16:52 -0600

    implement boilerplate for first integration test

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+describe('My First Test Suite', function() {
+  it('My First Test case', function() {
+    // test step
+  });
+  it('My Second Test case', function() {
+    // test step
+  });
+});
