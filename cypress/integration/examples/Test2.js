803ab41343ebbd742cf16f19fe6650238ea0db34
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-19 22:33:51 -0600

    finish funnel

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -7,8 +7,8 @@ describe('My Second Test Suite', function() {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
-    cy.get('.products').as('productLocation');
-    cy.get('@productLocation')
+    cy.get('.products').as('productLocator');
+    cy.get('@productLocator')
       .find('.product')
       .each(($e1, index, $list) => {
         const textVeg = $e1.find('h4.product-name').text();
@@ -17,5 +17,7 @@ describe('My Second Test Suite', function() {
         }
       });
     cy.get('.cart-icon > img').click();
+    cy.contains('PROCEED TO CHECKOUT').click();
+    cy.contains('Place Order').click();
   });
 });
