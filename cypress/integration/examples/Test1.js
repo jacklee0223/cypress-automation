83aea66f8359ee742a4cd2d3669b3db2e223b318
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-19 21:09:48 -0600

    implement more steps

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,6 +8,23 @@ describe('My First Test Suite', function() {
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
     cy.get('.product:visible').should('have.length', 4);
+    cy.get('.products')
+      .find('.product')
+      .should('have.length', 4);
+    cy.get(':nth-child(3) > .product-action > button').click();
+    cy.get('.products')
+      .find('.product')
+      .eq(2)
+      .contains('ADD TO CART')
+      .click();
+    cy.get('.products')
+      .find('.product')
+      .each(($e1, index, $list) => {
+        const textVeg = $e1.find('h4.product-name').text();
+        if (textVeg.includes('Cashews')) {
+          $e1.find('button').click();
+        }
+      });
     // fixture
   });
   it('My Second Test case', function() {
